babd48053d219412967cd8a762de989f67c1ae17
Naomi2300 <naomi.oropesa.623@gmail.com>
2025-10-21 19:40:59 -0400

    Update header.js

--- header.js
+++ header.js
@@ -10,19 +10,19 @@ class PrimaryHeader extends HTMLElement {
      <nav>
       <ul>
         <li><a href="/index.html">Home</a></li>
-        <li><a href="/events.html">Events</a></li>
-        <li><a href="/join.html">Join Us</a></li>
-        <li><a href="/resources.html">Resources</a></li>
-        <li><a href="/archive.html">Archive</a></li>
-        <li><a href="/rating.html">Rate Professors</a></li>
-        <li><a href="/contact.html">Contact</a></li>
+        <li><a href="./Pages/events.html">Events</a></li>
+        <li><a href="./Pages/join.html">Join Us</a></li>
+        <li><a href="./Pages/resources.html">Resources</a></li>
+        <li><a href="./Pages/archive.html">Archive</a></li>
+        <li><a href="./Pages/rating.html">Rate Professors</a></li>
+        <li><a href="./Pages/contact.html">Contact</a></li>
         <li>
           <a href="#">More</a>
           <ul class="dropdown">
-            <li><a href="/codingCh.html">Coding Challenges</a></li>
-            <li><a href="/careers.html">Careers</a></li>
-            <li><a href="/members.html">Meet The Members</a></li>
-            <li><a href="/board.html">Meet The Board</a></li>
+            <li><a href="./Pages/codingCh.html">Coding Challenges</a></li>
+            <li><a href="./Pages/careers.html">Careers</a></li>
+            <li><a href="./Pages/members.html">Meet The Members</a></li>
+            <li><a href="./Pages/board.html">Meet The Board</a></li>
           </ul>
         </li>
       </ul>
