008c3e63ac02ca1bbc14f4ac062f2d21fc1eac6f
Rahdyce <Re05028@georgiasouthern.edu>
2025-10-22 11:52:47 -0400

    Fixed the pathing for the header.js file to be root relative. Page naviagation should work as intended now.

--- header.js
+++ header.js
@@ -10,19 +10,19 @@ class PrimaryHeader extends HTMLElement {
      <nav>
       <ul>
         <li><a href="/index.html">Home</a></li>
-        <li><a href="./Pages/events.html">Events</a></li>
-        <li><a href="./Pages/join.html">Join Us</a></li>
-        <li><a href="./Pages/resources.html">Resources</a></li>
-        <li><a href="./Pages/archive.html">Archive</a></li>
-        <li><a href="./Pages/rating.html">Rate Professors</a></li>
-        <li><a href="./Pages/contact.html">Contact</a></li>
+        <li><a href="/Pages/events.html">Events</a></li>
+        <li><a href="/Pages/join.html">Join Us</a></li>
+        <li><a href="/Pages/resources.html">Resources</a></li>
+        <li><a href="/Pages/archive.html">Archive</a></li>
+        <li><a href="/Pages/rating.html">Rate Professors</a></li>
+        <li><a href="/Pages/contact.html">Contact</a></li>
         <li>
           <a href="#">More</a>
           <ul class="dropdown">
-            <li><a href="./Pages/codingCh.html">Coding Challenges</a></li>
-            <li><a href="./Pages/careers.html">Careers</a></li>
-            <li><a href="./Pages/members.html">Meet The Members</a></li>
-            <li><a href="./Pages/board.html">Meet The Board</a></li>
+            <li><a href="/Pages/codingCh.html">Coding Challenges</a></li>
+            <li><a href="/Pages/careers.html">Careers</a></li>
+            <li><a href="/Pages/members.html">Meet The Members</a></li>
+            <li><a href="/Pages/board.html">Meet The Board</a></li>
           </ul>
         </li>
       </ul>
