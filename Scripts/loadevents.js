76097b2ff43126b3769a0258defca2089fcc808c
Ethan H Cox <coxethan924@gmail.com>
2026-03-12 14:17:34 -0400

    Create loadevents.js

---
+++ Scripts/loadevents.js
@@ -0,0 +1,25 @@
+fetch("Data/events.json")
+.then(response => response.json())
+.then(data => {
+
+    const list = document.getElementById("events-list");
+
+    data.forEach(event => {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <strong>
+                <a href="${event.link}" target="_blank" rel="noopener noreferrer">
+                    ${event.title}
+                </a>:
+            </strong>
+            ${event.description} <em>${event.date}</em>
+        `;
+
+        list.appendChild(li);
+
+    });
+
+})
+.catch(err => console.error("Events failed to load:", err));
